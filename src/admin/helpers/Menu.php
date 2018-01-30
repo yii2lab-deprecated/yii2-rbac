5221483505806f637c782476a7b0d5759e5556aa
Vitaliy <theyamshikov@yandex.ru>
2018-01-30 15:17:08 +0600

    refacto menu

--- src/admin/helpers/Menu.php
+++ src/admin/helpers/Menu.php
@@ -2,13 +2,11 @@
 
 namespace yii2lab\rbac\admin\helpers;
 
-// todo: отрефакторить - сделать нормальный интерфейс и родителя
-
 use common\enums\rbac\PermissionEnum;
 
 class Menu {
 	
-	static function getMenu() {
+	public function toArray() {
 		return [
 			'label' => ['admin', 'rbac'],
 			'module' => 'rbac',
