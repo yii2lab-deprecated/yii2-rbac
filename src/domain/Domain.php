1b26267d1455c3a565539edcad84c19df1a0923b
Vitaliy <theyamshikov@yandex.ru>
2018-03-30 15:29:49 +0600

    make docblock in domain

--- src/domain/Domain.php
+++ src/domain/Domain.php
@@ -4,6 +4,14 @@ namespace yii2lab\rbac\domain;
 
 use yii2lab\domain\enums\Driver;
 
+/**
+ * Class Domain
+ *
+ * @package yii2lab\rbac\domain
+ *
+ * @property \yii2lab\rbac\domain\services\RuleService $rule
+ * @property \yii2lab\rbac\domain\services\ConstService $const
+ */
 class Domain extends \yii2lab\domain\Domain {
 	
 	public function config() {
