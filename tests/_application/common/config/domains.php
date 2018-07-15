c0a4b757e28876920690e45d8793ff1c12df9a47
Vitaliy <theyamshikov@yandex.ru>
2018-07-16 00:12:24 +0600

    refacto test

--- tests/_application/common/config/domains.php
+++ tests/_application/common/config/domains.php
@@ -1,48 +1,11 @@
 <?php
 
 use yii\helpers\ArrayHelper;
-use yii2lab\domain\Domain;
-use yii2lab\domain\enums\Driver;
 use yii2lab\test\helpers\TestHelper;
 
 $config = [
-	'rbac' => 'yii2lab\rbac\domain\Domain',
-	'account' => [
-		'class' => Domain::class,
-		'path' => 'yii2module\account\domain\v2',
-		'repositories' => [
-			'auth' => Driver::FILEDB,
-			'login' => Driver::FILEDB,
-			'registration' => Driver::FILEDB,
-			'temp' => Driver::ACTIVE_RECORD,
-			'restorePassword' => Driver::FILEDB,
-			'security' => Driver::FILEDB,
-			'test' => Driver::DISC,
-			'rbac' => Driver::MEMORY,
-			'confirm' => Driver::ACTIVE_RECORD,
-			'assignment' => Driver::FILEDB,
-			'token' => Driver::ACTIVE_RECORD,
-		],
-		'services' => [
-			'auth',
-			'login' => [
-				'prefixList' => ['B', 'BS', 'R', 'QRS'],
-				'defaultRole' => null,
-				'defaultStatus' => 1,
-				'forbiddenStatusList' => [0],
-			],
-			'registration',
-			'temp',
-			'restorePassword',
-			'security',
-			'test',
-			'balance',
-			'rbac',
-			'confirm',
-			'assignment',
-			'token',
-		],
-	],
+	'service' => 'yii2woop\service\domain\v3\Domain',
+	'summary' => 'yii2woop\summary\domain\Domain',
 ];
 
 $baseConfig = TestHelper::loadConfig('common/config/domains.php');
