277c811065f8fb573823be1ac8f46c1b7b9cd635
Vitaliy <theyamshikov@yandex.ru>
2018-07-23 18:32:39 +0600

    fix woop pathes

--- tests/_application/common/config/domains.php
+++ tests/_application/common/config/domains.php
@@ -4,8 +4,6 @@ use yii\helpers\ArrayHelper;
 use yii2lab\test\helpers\TestHelper;
 
 $config = [
-	'service' => 'yii2woop\service\domain\v3\Domain',
-	'summary' => 'yii2woop\summary\domain\Domain',
 ];
 
 $baseConfig = TestHelper::loadConfig('common/config/domains.php');
