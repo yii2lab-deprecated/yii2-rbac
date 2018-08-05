4397b7efa8dbc6d4ef3a599effb53d7378e73f91
Vitaliy <theyamshikov@yandex.ru>
2018-08-05 22:36:28 +0600

    fix test

--- tests/_application/common/config/domains.php
+++ tests/_application/common/config/domains.php
@@ -4,6 +4,7 @@ use yii\helpers\ArrayHelper;
 use yii2lab\test\helpers\TestHelper;
 
 $config = [
+	'rbac' => 'yii2lab\rbac\domain\Domain',
 ];
 
 $baseConfig = TestHelper::loadConfig('common/config/domains.php');
