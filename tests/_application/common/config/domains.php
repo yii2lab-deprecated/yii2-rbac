3bd88d1b3002dd02ffd7dde9435a281fbf864e84
Vitaliy <theyamshikov@yandex.ru>
2018-08-05 22:51:58 +0600

    fix test

--- tests/_application/common/config/domains.php
+++ tests/_application/common/config/domains.php
@@ -2,9 +2,22 @@
 
 use yii\helpers\ArrayHelper;
 use yii2lab\test\helpers\TestHelper;
+use yii2lab\domain\enums\Driver;
+use yii2lab\rbac\domain\repositories\disc\ItemRepository;
+use yii2lab\rbac\domain\repositories\disc\RuleRepository;
 
 $config = [
-	'rbac' => 'yii2lab\rbac\domain\Domain',
+    'rbac' => [
+	    'class' => 'yii2lab\rbac\domain\Domain',
+        'repositories' => [
+            'rule' => [
+                'ruleFile' => '@vendor/yii2lab/yii2-test/src/base/_application/common/data/rbac/rules.php',
+            ],
+            'item' => [
+                'itemFile' => '@vendor/yii2lab/yii2-test/src/base/_application/common/data/rbac/items.php',
+            ],
+        ],
+    ],
 ];
 
 $baseConfig = TestHelper::loadConfig('common/config/domains.php');
