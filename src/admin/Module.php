2a45edb891028f9452097fc097d0f4792b2d0851
Vitaliy <theyamshikov@yandex.ru>
2018-08-11 22:19:52 +0600

    upgrade rbac

---
+++ src/admin/Module.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace yii2lab\rbac\admin;
+
+use yii2lab\helpers\Behavior;
+use yii2lab\rbac\domain\enums\RbacPermissionEnum;
+
+class Module extends \mdm\admin\Module
+{
+
+    var $controllerNamespace = 'mdm\admin\controllers';
+    var $viewPath = '//vendor/mdmsoft/yii2-admin/views';
+    public $controllerMap = [
+        'assignment' => [
+            'class' => 'yii2lab\rbac\admin\controllers\AssignmentController',
+            'userClassName' => 'yii2module\account\domain\v2\models\User',
+            'usernameField' => 'login',
+        ],
+    ];
+
+    public function behaviors()
+    {
+        return [
+            'access' => Behavior::access(RbacPermissionEnum::MANAGE),
+        ];
+    }
+
+}
---
+++ src/admin/helpers/ModuleHelper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace yii2lab\rbac\admin\helpers;
+
+use yii2lab\helpers\Behavior;
+use yii2lab\rbac\domain\enums\RbacPermissionEnum;
+
+class ModuleHelper {
+	
+	public static function config() {
+		return [
+            'class' => 'mdm\admin\Module',
+            'controllerMap' => [
+                'assignment' => [
+                    'class' => 'yii2lab\rbac\admin\controllers\AssignmentController',
+                    'userClassName' => 'yii2module\account\domain\v2\models\User',
+                    'usernameField' => 'login',
+                ],
+            ],
+            'as access' => Behavior::access(RbacPermissionEnum::MANAGE),
+        ];
+	}
+	
+}
