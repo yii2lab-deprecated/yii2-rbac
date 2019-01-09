e72ac0359af660690c161451f864208b2d20919d
amubarak <amubarak@wooppay.com>
2019-01-09 14:54:24 +0600

    fix auth error

--- src/console/controllers/RoleController.php
+++ src/console/controllers/RoleController.php
@@ -2,33 +2,37 @@
 
 namespace yii2lab\rbac\console\controllers;
 
-use Yii;
 use yii\web\UnauthorizedHttpException;
 use yii2lab\extension\console\base\Controller;
 use yii2lab\extension\web\helpers\Behavior;
+use yii2woop\generated\exception\tps\NotAuthenticatedException;
 
-class RoleController extends Controller
-{
+class RoleController extends Controller {
 	
-	public function behaviors()
-	{
+	public function behaviors() {
 		return [
 			'authenticator' => Behavior::auth(),
 		];
 	}
+	
 	/**
 	 * Search and add RBAC rules
 	 */
-	public function actionUpdate()
-	{
-		try{
+	public function actionUpdate() {
+		
+		try {
 			\App::$domain->rbac->role->update();
-		}catch(UnauthorizedHttpException $e){
-			Yii::$app->cache->set('identity', null);
+		} catch(NotAuthenticatedException $e) {
 			\App::$domain->account->auth->breakSession();
+			echo 'NotAuthenticatedException';
+			return;
+		} catch(UnauthorizedHttpException $e) {
+			\App::$domain->account->auth->breakSession();
+			echo 'UnauthorizedHttpException';
+			return;
 		}
 		echo 'success';
 	}
-
+	
 	
 }
