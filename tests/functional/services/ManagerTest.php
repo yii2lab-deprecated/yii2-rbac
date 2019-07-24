f3ee36002c36a9549c55b6d6c7ca413c8d6ea3c4
amubarak <amubarak@wooppay.com>
2019-07-24 14:08:31 +0600

    add fixtures

--- tests/functional/services/ManagerTest.php
+++ tests/functional/services/ManagerTest.php
@@ -2,6 +2,8 @@
 
 namespace tests\functional\services;
 
+use yii2lab\test\fixtures\UserAssignmentFixture;
+use yii2lab\test\fixtures\UserFixture;
 use yii2lab\test\Test\Unit;
 use yii\web\ForbiddenHttpException;
 use yii2module\account\domain\v2\helpers\TestAuthHelper;
@@ -10,6 +12,20 @@ class ManagerTest extends Unit
 {
 	const ID_ADMIN = 381949;
 	const ID_USER = 381070;
+
+	public function _before()
+	{
+		$this->tester->haveFixtures([
+			[
+				'class' => UserFixture::class,
+				'dataFile' => '@vendor/yii2lab/yii2-test/src/fixtures/data/user.php'
+			],
+			[
+				'class' => UserAssignmentFixture::class,
+				'dataFile' => '@vendor/yii2lab/yii2-test/src/fixtures/data/user_assignment.php'
+			],
+		]);
+	}
 
 	public function testCanByUser()
 	{
